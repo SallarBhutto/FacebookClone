513e738d69b9bebddd2b07fd5d4b65abda815d6c
Hussain Bhutto <sallar.hussain@afiniti.com>
2020-10-09 19:14:09 +0500

    UI Complete

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,10 @@ import "./Feed.css";
 import MessageSender from "./MessageSender";
 import Post from "./Post";
 import StoryReel from "./StoryReel";
+
+const picture =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTaCcvzDeiaAOP-cFSz0OJ0XvlR83YzYRXl_g&usqp=CAU";
+
 export default function Feed() {
   return (
     <div className="feed">
@@ -10,7 +14,20 @@ export default function Feed() {
       {/* MessageSender */}
       <MessageSender />
 
-      <Post />
+      <Post
+        profilePic={picture}
+        message={"wow this works"}
+        timestamp={new Date()}
+        username={"sallar bhutto"}
+        image={"https://i.ytimg.com/vi/a0Xm1OahWQA/maxresdefault.jpg"}
+      />
+
+      <Post
+        profilePic={picture}
+        message={"wow this works"}
+        timestamp={new Date()}
+        username={"sallar bhutto"}
+      />
     </div>
   );
 }
